bd7fc11ea2b394c9cba71ef88d80b34a25fbc8a1
rockintuna <rockintuna@naver.com>
2020-08-12 23:47:22 +0900

    spring IoC

---
+++ SpringBoot_Project_1/make_post.sh
@@ -0,0 +1,19 @@
+export FILENAMES=`ls *.md | grep -v readme.md`
+export DIR=/Users/ijeong-in
+for FILENAME in $FILENAMES
+do
+export NEWFILENAME=`echo "hexo_"${FILENAME}`
+
+echo "---" > ${DIR}/target.out
+echo "title: "`cat $FILENAME | grep '####' | sed 's/#### //g'` >> ${DIR}/target.out
+echo "date: "`date '+%Y-%m-%d %H:%M:%S'` >> ${DIR}/target.out
+echo "categories: " >> ${DIR}/target.out
+echo " - Study" >> ${DIR}/target.out
+echo " - Spring Boot Project" >> ${DIR}/target.out
+echo "tags: spring boot" >> ${DIR}/target.out
+echo "---" >> ${DIR}/target.out
+
+cat $FILENAME >> ${DIR}/target.out
+
+mv ${DIR}/target.out /Users/ijeong-in/Git_repo/hexoblog/source/_posts/${NEWFILENAME}
+done
